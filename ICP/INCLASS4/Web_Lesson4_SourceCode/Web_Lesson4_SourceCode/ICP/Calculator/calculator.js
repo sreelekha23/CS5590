020e73b822652b8ea4a2b9fd6c3c5872da27228c
sreelekha23 <sreelekha.kreddy@gmail.com>
2018-02-10 15:44:25 -0600

    commit

---
+++ ICP/INCLASS4/Web_Lesson4_SourceCode/Web_Lesson4_SourceCode/ICP/Calculator/calculator.js
@@ -0,0 +1,20 @@
+angular.module('CalculatorApp', [])
+    .controller('CalculatorController', function ($scope) {
+        // Write code simple calculator operations
+
+        $scope.result = function() {
+            if ($scope.operator == '+') {
+                return $scope.a + $scope.b;
+            }
+            if ($scope.operator == '-') {
+                return $scope.a - $scope.b;
+            }
+            if ($scope.operator == '*') {
+                return $scope.a * $scope.b;
+            }
+            if ($scope.operator == '/') {
+                return $scope.a / $scope.b;
+            }
+        };
+
+    });
